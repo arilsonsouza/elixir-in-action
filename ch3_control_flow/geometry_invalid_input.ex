55b3808b1895f6e512bf2b499113e5d834f1d0a6
Arilson Souza <arilsons61@gmail.com>
2021-01-21 10:48:44 -0300

    Multiclause function invalid input

---
+++ ch3_control_flow/geometry_invalid_input.ex
@@ -0,0 +1,12 @@
+defmodule Geometry do
+  @moduledoc """
+    Multiclause function invalid input
+  """
+  def area({:rectangle, a, b}), do: a * b
+
+  def area({:square, a}), do: a * a
+
+  def area({:circle, r}), do: r * r * 3.14
+
+  def area(unknown), do: {:error, {:unknown_shape, unknown}}
+end
