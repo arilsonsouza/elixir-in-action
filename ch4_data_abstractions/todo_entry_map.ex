928d6463a323983fd4dab553ec25cf556c380b5c
Arilson Souza <arilsons61@gmail.com>
2021-01-26 15:38:19 -0300

    Defining a structure

--- ch4_data_abstractions/todo_entry_map.ex
+++ ch4_data_abstractions/todo_entry_map.ex
@@ -2,7 +2,7 @@ defmodule MultiDict do
   def new(), do: %{}
 
   def add(dict, key, value) do
-    Map.update(dic, key, [value], &[value | &1])
+    Map.update(dict, key, [value], &[value | &1])
   end
 
   def get(dict, key) do
